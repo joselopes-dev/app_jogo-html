a4ceec706eafbe70754bb70d4e10c465d96802da
joselopes-dev <140862898+joselopes-dev@users.noreply.github.com>
2025-03-16 00:10:22 -0400

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,42 @@
+const CACHE_NAME = 'Jogo-HTML';
+const urlsToCache = [
+    '/',
+    '/index.html',
+    '/stylejogo_card.css',
+    '/scriptjogo_card.js',
+    '/icon-192x192.png',
+    '/icon-512x512.png'
+];
+
+// Instalando o Service Worker e adicionando os arquivos ao cache
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => {
+            return cache.addAll(urlsToCache);
+        })
+    );
+});
+
+// Interceptando requisições e servindo do cache quando possível
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request).then((response) => {
+            return response || fetch(event.request);
+        })
+    );
+});
+
+// Atualizando o cache quando há mudanças
+self.addEventListener('activate', (event) => {
+    event.waitUntil(
+        caches.keys().then((cacheNames) => {
+            return Promise.all(
+                cacheNames.map((cache) => {
+                    if (cache !== CACHE_NAME) {
+                        return caches.delete(cache);
+                    }
+                })
+            );
+        })
+    );
+});
